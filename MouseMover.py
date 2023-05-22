dbc7ce651e4726749f7e32eea8c59187a9e09d4b
조영준 <shootinger@naver.com>
2023-05-22 14:01:35 +0900

    랜덤 포지션과 마우스 클릭 추가

--- MouseMover.py
+++ MouseMover.py
@@ -5,6 +5,17 @@ import keyboard
 
 Loop = True
 count = 0
+pag.FAILSAFE = False
+
+def get_random_coords():
+    screen = pag.size()
+    width = screen[0]
+    height = screen[1]
+
+    return [
+        random.randint(100,width-200),
+        random.randint(100,height-200)
+    ]
 
 while Loop:
 
@@ -15,8 +26,12 @@ while Loop:
     #y = random.randint(400,450)
     if count == 100:
         #현재위치
-        x = pag.position().x
-        y = pag.position().y
+        #x = pag.position().x
+        #y = pag.position().y
+        coord = get_random_coords()
+        x = coord[0]
+        y = coord[1]
+        pag.moveTo(x,y,0.2)
 
         #다이아몬드 
         x -= 30
@@ -34,6 +49,8 @@ while Loop:
         x -= 30
         y -= 30
         pag.moveTo(x,y,0.2)
+
+        pag.click()
     
         count = 0
     
